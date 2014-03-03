1eec52320e05cd0c03781c159b24246078bc0583
Manish Goregaokar <manishsmail@gmail.com>
2014-03-03 08:49:32 +0530

    icons!!

--- background.js
+++ background.js
@@ -3,26 +3,33 @@ chrome.tabs.onRemoved.addListener(function(id,stuff){
 })
 function updateBadges(){
 chrome.storage.sync.get(null,function(keys){
-chrome.tabs.query({},function(tabs){
-for (tab in tabs){
-tabid=tabs[tab].id;
-tabURL=tabs[tab].url;
-console.log(tabs[tab])
- if(keys["tab-"+tabid] || keys["url-"+tabURL]){
-   chrome.browserAction.setBadgeText({text:"!",tabId:tabid})
- }
-}
-})
+  chrome.tabs.query({},function(tabs){
+    for (tab in tabs){
+      tabid=tabs[tab].id;
+      tabURL=tabs[tab].url;
+      updateTab(keys,tabURL,tabid)
+
+    }
+  })
 });
 }
 
 function updatePageBadge(a,b,tab){
   console.log(tab);
   chrome.storage.sync.get(["tab-"+tab.id,"url-"+tab.url],function(keys){
-  if(keys["tab-"+tab.id] || keys["url-"+tab.url]){
-    chrome.browserAction.setBadgeText({text:"!",tabId:tab.id})
-  }
+  updateTab(keys,tab.url,tab.id)
 });
+}
+
+function updateTab(keys,url,id){
+  
+  if(keys["tab-"+id] || keys["url-"+url]){
+   //chrome.browserAction.setBadgeText({text:"!",tabId:id})
+   chrome.browserAction.setIcon({path:{"19":"icontildalactive19.png","38":"icontildalactive19.png"},tabId:id})
+  }else{
+    //chrome.browserAction.setBadgeText({text:"",tabId:id})
+   chrome.browserAction.setIcon({path:{"19":"icontildal19.png","38":"icontildal19.png"},tabId:id})
+  }
 }
 chrome.storage.onChanged.addListener(updateBadges);
 chrome.tabs.onUpdated.addListener(updatePageBadge)
