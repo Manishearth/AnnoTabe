28eaf02c9f75433c55986456ae463d3aff6081f3
Manish Goregaokar <manishsmail@gmail.com>
2014-03-03 08:09:45 +0530

    add badges

--- background.js
+++ background.js
@@ -1,3 +1,28 @@
 chrome.tabs.onRemoved.addListener(function(id,stuff){
   chrome.storage.sync.remove(['tab-'+id],function(){})
 })
+function updateBadges(){
+chrome.storage.sync.get(null,function(keys){
+chrome.tabs.query({},function(tabs){
+for (tab in tabs){
+tabid=tabs[tab].id;
+tabURL=tabs[tab].url;
+console.log(tabs[tab])
+ if(keys["tab-"+tabid] || keys["url-"+tabURL]){
+   chrome.browserAction.setBadgeText({text:"!",tabId:tabid})
+ }
+}
+})
+});
+}
+
+function updatePageBadge(a,b,tab){
+  console.log(tab);
+  chrome.storage.sync.get(["tab-"+tab.id,"url-"+tab.url],function(keys){
+  if(keys["tab-"+tab.id] || keys["url-"+tab.url]){
+    chrome.browserAction.setBadgeText({text:"!",tabId:tab.id})
+  }
+});
+}
+chrome.storage.onChanged.addListener(updateBadges);
+chrome.tabs.onUpdated.addListener(updatePageBadge)
